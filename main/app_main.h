da15d996c6255c770b843e53029f166999a2847a
Shinika Balasundar <98338989+shibcreate@users.noreply.github.com>
2024-11-06 14:12:05 -0800

    working files -- test

---
+++ main/app_main.h
@@ -0,0 +1,24 @@
+#ifndef APP_MAIN_H
+#define APP_MAIN_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_system.h"
+#include "esp_partition.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+#include "esp_tls.h"
+#include "esp_ota_ops.h"
+#include <sys/param.h>
+
+// Function to start the MQTT client
+void mqtt_app_start(char buff[]);
+
+#endif 
